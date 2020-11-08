a38cea84799b81d8ed2fa4097bf00cac1df2a7e5
justinyugit <justin02yu@gmail.com>
2020-11-07 23:42:32 -0500

    can now check if product input is correct and stops alarm

--- runAlarm.sh
+++ runAlarm.sh
@@ -6,14 +6,15 @@ read minute
 read -p "Confirm your alarm @ $hour:$minute? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 while true; do
     currHour=$(date +%H)
-    currMin=$(date +%M)
-    
+    currMin=$(date +%M)    
     if [ $currHour -eq $hour ] && [ $currMin -eq $minute ]; then
 	number1=$((RANDOM%10))
 	number2=$((RANDOM%10))
-	echo $number1
-	echo $number2
-	$(mpg123 -q /home/pi/math-alarm/alarm.mp3)
+	product=$(($number1*$number2))
+	figlet $number1 x $number2 = ?
+
+	$(mpg123 -q /home/pi/math-alarm/alarm.mp3) &
+	read -p "Product: " input && [ $input -eq $product ] && killall mpg123 && exit 1 || continue
     fi
     sleep 20
 done
