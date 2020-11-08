ff89644e7188802f17e341218bfa16fa63f4e4b5
justinyugit <justin02yu@gmail.com>
2020-11-07 22:34:01 -0500

    added mp3 file and confirmation of selection time

--- alarm.sh
+++ alarm.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+echo "Set Alarm Hour: "
+read hour
+echo "Set Alarm Minute: "
+read minute
+read -p "Confirm your alarm @ $hour:$minute? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+echo "YES"
